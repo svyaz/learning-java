491204e668674c49b4f92152277d6ff002049659
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-01-16 23:09:51 +0700

    Релизован класс Rectangle и тесты для него.

---
+++ Shapes/src/com/github/svyaz/javalearning/shapes/Rectangle.java
@@ -0,0 +1,51 @@
+package com.github.svyaz.javalearning.shapes;
+
+public class Rectangle implements Shape {
+    private static final String EXCEPTION_MESSAGE_BOTH = "'width' and 'height' must be greater than 0.";
+    private static final String EXCEPTION_MESSAGE_WIDTH = "'width' must be greater than 0.";
+    private static final String EXCEPTION_MESSAGE_HEIGHT = "'height' must be greater than 0.";
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) throws IllegalArgumentException {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_BOTH);
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) throws IllegalArgumentException {
+        if (width <= 0) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_WIDTH);
+        }
+        this.width = width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) throws IllegalArgumentException {
+        if (height <= 0) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_HEIGHT);
+        }
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+}
---
+++ Shapes/test/RectangleTest.java
@@ -0,0 +1,68 @@
+import com.github.svyaz.javalearning.shapes.Rectangle;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RectangleTest {
+    private static final double EPSILON = 1e-10;
+
+    @Test
+    public void constructorTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Assert.assertTrue(rectangle.getWidth() == 2 && rectangle.getHeight() == 3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorExceptionTest() {
+        new Rectangle(-2, -3);
+    }
+
+    @Test
+    public void getWidthTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Assert.assertEquals(rectangle.getWidth(), 2, EPSILON);
+    }
+
+    @Test
+    public void setWidthTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        rectangle.setWidth(2.5);
+        Assert.assertEquals(rectangle.getWidth(), 2.5, EPSILON);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setWidthExceptionTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        rectangle.setWidth(-0.5);
+    }
+
+    @Test
+    public void getHeightTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Assert.assertEquals(rectangle.getHeight(), 3, EPSILON);
+    }
+
+    @Test
+    public void setHeightTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        rectangle.setHeight(2.5);
+        Assert.assertEquals(rectangle.getHeight(), 2.5, EPSILON);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setHeightExceptionTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        rectangle.setHeight(-0.5);
+    }
+
+    @Test
+    public void getAreaTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Assert.assertEquals(rectangle.getArea(), 6, EPSILON);
+    }
+
+    @Test
+    public void getPerimeterTest() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Assert.assertEquals(rectangle.getPerimeter(), 10, EPSILON);
+    }
+}
