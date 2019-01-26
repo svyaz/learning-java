7a7f4061bce732f24347f6f830c1138d6038cd38
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-01-26 17:09:49 +0700

    Модуль Matrix. Файлы для класса и тестов.

---
+++ Matrix/src/com/github/svyaz/javalearning/matrix/Matrix.java
@@ -0,0 +1,7 @@
+package com.github.svyaz.javalearning.matrix;
+
+import com.github.svyaz.javalearning.vector.Vector;
+
+public class Matrix {
+    private Vector[] rows;
+}
---
+++ Matrix/test/MatrixTest.java
@@ -0,0 +1,10 @@
+import com.github.svyaz.javalearning.matrix.Matrix;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MatrixTest {
+    @Test
+    public void constructor1Test() {
+        //Matrix matrix = new Matrix(2, 3);
+    }
+}
