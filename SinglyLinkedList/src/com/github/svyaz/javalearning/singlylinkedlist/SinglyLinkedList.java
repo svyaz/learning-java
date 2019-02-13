2a33acda7119d22e5402e7cd11bd5fdd18298155
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-02-13 19:21:47 +0700

    SinglyLinkedList - создан модуль и файл для тестов.

---
+++ SinglyLinkedList/src/com/github/svyaz/javalearning/singlylinkedlist/SinglyLinkedList.java
@@ -0,0 +1,4 @@
+package com.github.svyaz.javalearning.singlylinkedlist;
+
+public class SinglyLinkedList {
+}
---
+++ SinglyLinkedList/test/SinglyLinkedListTest.java
@@ -0,0 +1,7 @@
+import com.github.svyaz.javalearning.singlylinkedlist.SinglyLinkedList;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SinglyLinkedListTest {
+
+}
