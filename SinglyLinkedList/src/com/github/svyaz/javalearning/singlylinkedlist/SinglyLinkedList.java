27c677d97236137cdea2cf2118143ec0a830b8eb
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-02-13 20:22:43 +0700

    SinglyLinkedList - конструкторы и три теста.

--- SinglyLinkedList/src/com/github/svyaz/javalearning/singlylinkedlist/SinglyLinkedList.java
+++ SinglyLinkedList/src/com/github/svyaz/javalearning/singlylinkedlist/SinglyLinkedList.java
@@ -1,4 +1,58 @@
 package com.github.svyaz.javalearning.singlylinkedlist;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    /**
+     * Head item of the list.
+     */
+    private ListItem<T> head;
+
+    /**
+     * Counter for getting list size.
+     */
+    private int count;
+
+    /**
+     * Creates SinglyLinkedList with 1-st item head.
+     */
+    public SinglyLinkedList(ListItem<T> head) {
+        if (head != null) {
+            this.head = head;
+            count = 1;
+            ListItem<T> tmpListItem = head;
+            while (tmpListItem.getNext() != null) {
+                tmpListItem = tmpListItem.getNext();
+                ++count;
+            }
+        }
+    }
+
+    /**
+     * Creates SinglyLinkedList with 1 item specified as data.
+     */
+    public SinglyLinkedList(T head) {
+        if (head != null) {
+            this.head = new ListItem<>(head, null);
+            count = 1;
+        }
+    }
+
+    /**
+     * Returns size of the list.
+     */
+    public int size() {
+        return count;
+    }
+
+    /**
+     * Returns head-data of the list.
+     */
+    public T getHeadData() {
+        return head.getData();
+    }
+
+
+
+    // TODO Implement equals
+    // TODO Implement hashCode
+    // TODO Implement toString
 }
--- SinglyLinkedList/test/SinglyLinkedListTest.java
+++ SinglyLinkedList/test/SinglyLinkedListTest.java
@@ -1,7 +1,32 @@
+import com.github.svyaz.javalearning.singlylinkedlist.ListItem;
 import com.github.svyaz.javalearning.singlylinkedlist.SinglyLinkedList;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SinglyLinkedListTest {
+    @Test
+    public void constructor1Items1Test() {
+        ListItem<String> startItem = new ListItem<>("Hi there", null);
+        SinglyLinkedList<String> list = new SinglyLinkedList<>(startItem);
+        Assert.assertTrue(list.size() == 1 && list.getHeadData().equals("Hi there"));
+    }
+
+    // TODO check all 3 elements
+    @Test
+    public void constructor1Items3Test() {
+        ListItem<String> thirdItem = new ListItem<>("3 item", null);
+        ListItem<String> secondItem = new ListItem<>("2 item", thirdItem);
+        ListItem<String> firstItem = new ListItem<>("1 item", secondItem);
+        SinglyLinkedList<String> list = new SinglyLinkedList<>(firstItem);
+        Assert.assertTrue(list.size() == 3);
+    }
+
+    @Test
+    public void constructor2Test() {
+        SinglyLinkedList<String> list = new SinglyLinkedList<>("Hi there");
+        Assert.assertTrue(list.size() == 1 && list.getHeadData().equals("Hi there"));
+    }
+
+
 
 }
