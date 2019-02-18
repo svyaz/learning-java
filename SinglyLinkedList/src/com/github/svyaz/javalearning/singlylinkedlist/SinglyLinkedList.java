f779ad4fbdfb1fc36e6a30946f9853ab69b8ae2a
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-02-19 00:28:15 +0700

    SinglyLinkedList - исправлен add() с использованием getListItem.

--- SinglyLinkedList/src/com/github/svyaz/javalearning/singlylinkedlist/SinglyLinkedList.java
+++ SinglyLinkedList/src/com/github/svyaz/javalearning/singlylinkedlist/SinglyLinkedList.java
@@ -100,22 +100,16 @@ public class SinglyLinkedList<T> {
             throw new IndexOutOfBoundsException(EXCEPTION_MESSAGE_INDEX_OUT_OF_BOUNDS);
         }
 
-        ListItem<T> current = head, prev = null;
-        for (int i = 0; i <= count; i++) {
-            if (i == index) {
-                if (i == 0) {
-                    head = new ListItem<>(data, head);
-                } else if (i == count) {
-                    prev.setNext(new ListItem<>(data));
-                } else {
-                    prev.setNext(new ListItem<>(data, current));
-                }
-                ++count;
-                break;
-            }
-            prev = current;
-            current = current.getNext();
+        if (index == 0) {
+            head = new ListItem<>(data, head);
+        } else if (index == count) {
+            ListItem<T> currentTail = getListItem(count - 1);
+            currentTail.setNext(new ListItem<>(data));
+        } else {
+            ListItem<T> previousItem = getListItem(index - 1);
+            previousItem.setNext(new ListItem<>(data, previousItem.getNext()));
         }
+        ++count;
     }
 
     /**
