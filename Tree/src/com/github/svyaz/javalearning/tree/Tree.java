41d455b5b2122beb6731c30dd7f76a352adcb2e0
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-02-28 09:20:45 +0700

    Tree - файлы класса Tree и для тестов.

---
+++ Tree/src/com/github/svyaz/javalearning/tree/Tree.java
@@ -0,0 +1,5 @@
+package com.github.svyaz.javalearning.tree;
+
+public class Tree {
+
+}
---
+++ Tree/test/TreeTest.java
@@ -0,0 +1,7 @@
+import com.github.svyaz.javalearning.tree.Tree;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TreeTest {
+
+}
