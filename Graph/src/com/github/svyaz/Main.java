de24afc68c0d381ea197e0f53ac346698cc7b130
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-03-10 22:54:21 +0700

    Graph - методы для обхода графа в ширину и в глубину.

--- Graph/src/com/github/svyaz/Main.java
+++ Graph/src/com/github/svyaz/Main.java
@@ -3,24 +3,23 @@ package com.github.svyaz;
 import com.github.svyaz.javalearning.graph.Graph;
 import com.github.svyaz.javalearning.graph.Vertex;
 
-//@SuppressWarnings("unchecked")
+@SuppressWarnings("unchecked")
 public class Main {
     public static void main(String[] args) {
-
         // Graph ...
         Vertex<String>[] cities = (Vertex<String>[]) new Vertex[12];
-        cities[0] = new Vertex<>("Moscow");
-        cities[1] = new Vertex<>("London");
-        cities[2] = new Vertex<>("Tokyo");
-        cities[3] = new Vertex<>("Berlin");
-        cities[4] = new Vertex<>("Sao Paolo");
-        cities[5] = new Vertex<>("New York");
-        cities[6] = new Vertex<>("Sidney");
-        cities[7] = new Vertex<>("Singapore");
-        cities[8] = new Vertex<>("Paris");
-        cities[9] = new Vertex<>("Baku");
-        cities[10] = new Vertex<>("Beijing");
-        cities[11] = new Vertex<>("Rome");
+        cities[0] = new Vertex<>("0 - Moscow");
+        cities[1] = new Vertex<>("1 - London");
+        cities[2] = new Vertex<>("2 - Tokyo");
+        cities[3] = new Vertex<>("3 - Berlin");
+        cities[4] = new Vertex<>("4 - Sao Paolo");
+        cities[5] = new Vertex<>("5 - New York");
+        cities[6] = new Vertex<>("6 - Sidney");
+        cities[7] = new Vertex<>("7 - Singapore");
+        cities[8] = new Vertex<>("8 - Paris");
+        cities[9] = new Vertex<>("9 - Baku");
+        cities[10] = new Vertex<>("10 - Beijing");
+        cities[11] = new Vertex<>("11 - Rome");
 
         int[][] adjMatrix = new int[cities.length][cities.length];
         adjMatrix[0] = new int[]{0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0};
@@ -37,5 +36,13 @@ public class Main {
         adjMatrix[11] = new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0};
 
         Graph<String> cityGraph = new Graph<>(cities, adjMatrix);
+
+        System.out.println("Traversing by width:");
+        cityGraph.forEachByWidth(s -> System.out.println(s.toUpperCase()));
+        System.out.println();
+
+        System.out.println("Traversing by depth:");
+        cityGraph.forEachByDepth(s -> System.out.println(s.toUpperCase()));
+        System.out.println();
     }
 }
--- Graph/src/com/github/svyaz/javalearning/graph/Graph.java
+++ Graph/src/com/github/svyaz/javalearning/graph/Graph.java
@@ -1,5 +1,10 @@
 package com.github.svyaz.javalearning.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.function.Consumer;
+
 public class Graph<T> {
     private final static String EXC_MSG_INDEX_OUT_OF_BOUNDS = "Specified index is out of bounds of vertices.";
     /**
@@ -44,4 +49,67 @@ public class Graph<T> {
         return (T) vertices[index];
     }
 
+    /**
+     * Performs specified action for each element in the graph.
+     * Traversing by width.
+     *
+     * @param action to perform.
+     */
+    public void forEachByWidth(Consumer<T> action) {
+        int dim = vertices.length;
+        boolean[] visited = new boolean[dim];
+
+        for (int i = 0; i < dim; i++) {     // проход по несвязанным частям графа
+            if (visited[i]) {
+                continue;
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(i);
+            visited[i] = true;
+
+            while (!queue.isEmpty()) {  // проход по очереди
+                int index = queue.remove();
+                action.accept(vertices[index].getData());   // действие с элементом
+                for (int j = 0; j < dim; j++) { // проход по непосещенным элементам
+                    if (!visited[j] && adjMatrix[index][j] == 1 && index != j) {
+                        queue.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Performs specified action for each element in the graph.
+     * Traversing by depth.
+     *
+     * @param action to perform.
+     */
+    public void forEachByDepth(Consumer<T> action) {
+        int dim = vertices.length;
+        boolean[] visited = new boolean[dim];
+
+        for (int i = 0; i < dim; i++) {     // проход по несвязанным частям графа
+            if (visited[i]) {
+                continue;
+            }
+
+            Stack<Integer> stack = new Stack<>();
+            stack.push(i);
+            visited[i] = true;
+
+            while (!stack.isEmpty()) {  // проход по очереди
+                int index = stack.pop();
+                action.accept(vertices[index].getData());   // действие с элементом
+                for (int j = 0; j < dim; j++) { // проход по непосещенным элементам
+                    if (!visited[j] && adjMatrix[index][j] == 1 && index != j) {
+                        stack.push(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+    }
 }
