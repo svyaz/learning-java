423140c2b52b8c028c5ef04624f231915d863ddc
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-03-27 00:54:25 +0700

    Multithreading - Task1.

---
+++ Multithreading/src/com/github/svyaz/Main.java
@@ -0,0 +1,14 @@
+package com.github.svyaz;
+
+import com.github.svyaz.javalearning.multithreading.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Task 1
+        System.out.println("Task 1:");
+        Thread task1Thread = new Thread(new Task1());
+        task1Thread.start();
+        System.out.println("Main thread end.");
+        System.out.println();
+    }
+}
---
+++ Multithreading/src/com/github/svyaz/javalearning/multithreading/Task1.java
@@ -0,0 +1,15 @@
+package com.github.svyaz.javalearning.multithreading;
+
+public class Task1 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
