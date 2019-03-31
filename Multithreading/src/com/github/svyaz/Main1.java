4c6d1f818a96f9a35347c50ad94056c8062ce1fb
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-04-01 00:58:42 +0700

    Multithreading - task 1, main поток должен дожидаться когда завершится созданный.

--- Multithreading/src/com/github/svyaz/Main1.java
+++ Multithreading/src/com/github/svyaz/Main1.java
@@ -5,8 +5,12 @@ import com.github.svyaz.javalearning.multithreading.Task1;
 public class Main1 {
     public static void main(String[] args) {
         // Task 1
-        Thread newThread = new Thread(new Task1());
-        newThread.start();
-        System.out.println("Main thread ends.");
+        try {
+            Thread newThread = new Thread(new Task1());
+            newThread.start();
+            newThread.join();
+        } catch (InterruptedException ignore) {
+        }
+        System.out.println("Main thread continues.");
     }
 }
