ba37549855dd1a1c1a2ab296133f11c18e95aa6e
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-04-13 01:59:16 +0700

    ProducerConsumer - реализация классов модуля.

--- ProducerConsumer/src/com/github/svyaz/javalearning/producerconsumer/Consumer.java
+++ ProducerConsumer/src/com/github/svyaz/javalearning/producerconsumer/Consumer.java
@@ -1,6 +1,14 @@
 package com.github.svyaz.javalearning.producerconsumer;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Consumer implements Runnable {
+    private final ProducerConsumerManager manager;
+
+    public Consumer(ProducerConsumerManager manager) {
+        this.manager = manager;
+    }
+
     /**
      * When an object implementing interface <code>Runnable</code> is used
      * to create a thread, starting the thread causes the object's
@@ -14,6 +22,14 @@ public class Consumer implements Runnable {
      */
     @Override
     public void run() {
-
+        while (true) {
+            try {
+                manager.getItem();
+                int randomValue = ThreadLocalRandom.current().nextInt(1000, 2000);
+                Thread.sleep(randomValue);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
     }
 }
--- ProducerConsumer/src/com/github/svyaz/javalearning/producerconsumer/ProducerConsumerManager.java
+++ ProducerConsumer/src/com/github/svyaz/javalearning/producerconsumer/ProducerConsumerManager.java
@@ -1,4 +1,60 @@
 package com.github.svyaz.javalearning.producerconsumer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ProducerConsumerManager {
+    private final Queue<String> queue = new LinkedList<>();
+    private final Object lock = new Object();
+    private final static int CAPACITY = 5;
+    private final static int PRODUCERS_COUNT = 2;
+    private final static int CONSUMERS_COUNT = 2;
+
+    /**
+     * Creates and starts producers and consumers threads.
+     */
+    public void start() {
+        for (int i = 0; i < PRODUCERS_COUNT; i++) {
+            Thread producer = new Thread(new Producer(this));
+            producer.setName("Producer-" + i);
+            producer.start();
+        }
+        for (int i = 0; i < CONSUMERS_COUNT; i++) {
+            Thread consumer = new Thread(new Consumer(this));
+            consumer.setName("Consumer-" + i);
+            consumer.start();
+        }
+    }
+
+    /**
+     * Gets an item from the queue.
+     *
+     * @return item from the queue
+     * @throws InterruptedException
+     */
+    public String getItem() throws InterruptedException {
+        synchronized (lock) {
+            while (queue.size() < CAPACITY) {
+                lock.wait();
+            }
+            String result = queue.remove();
+            lock.notifyAll();
+            return result;
+        }
+    }
+
+    /**
+     * Puts an item to the queue.
+     *
+     * @throws InterruptedException
+     */
+    public void addItem(String item) throws InterruptedException {
+        synchronized (lock) {
+            while (queue.size() >= CAPACITY) {
+                lock.wait();
+            }
+            queue.add(item);
+            lock.notifyAll();
+        }
+    }
 }
